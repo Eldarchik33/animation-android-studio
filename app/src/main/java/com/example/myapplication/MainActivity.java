7084127b964a4b3eb18b6f96cacbcb535d4b2ade
Eldarchik33 <eldarchik.game@gmail.com>
2022-10-22 10:59:00 +0300

    анимация солнца

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Получим ссылку на солнце
+        ImageView sunImageView = findViewById(R.id.sun);
+        // Анимация для восхода солнца
+        Animation sunRiseAnimation = AnimationUtils.loadAnimation(this, R.anim.sun_rise);
+        // Подключаем анимацию к нужному View
+        sunImageView.startAnimation(sunRiseAnimation);
+    }
+}
