14495027fbdddc507ce11578c5831303a74917e7
Eldarchik33 <eldarchik.game@gmail.com>
2022-10-27 15:03:57 +0300

    update

--- app/src/androidTest/java/ru/startandroid/develop/loginpass/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/ru/startandroid/develop/loginpass/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.myapplication;
+package ru.startandroid.develop.loginpass;
 
 import android.content.Context;
 
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.myapplication", appContext.getPackageName());
+        assertEquals("ru.startandroid.develop.loginpass", appContext.getPackageName());
     }
 }
